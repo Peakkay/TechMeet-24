852dd826a9795b683ab785fe18aa8e72e24dfcf7
Peakkay <prachet.09.10@gmail.com>
2024-12-05 21:42:58 +0530

    Room Swaps finalized

--- Assets/Scripts/RoomSwapper.cs
+++ Assets/Scripts/RoomSwapper.cs
@@ -1,17 +1,18 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class RoomSwapper : MonoBehaviour
+public class RoomSwapper : MonoBehaviour,IInteractable
 {
     public GameObject mainGameObject; // The main game object to transfer between scenes
     public Vector3 playerTargetPositionInScene2; // Target position for the player in Scene 2
     public string targetSceneName; // The name of the scene to load
-
     private GameObject player;
 
     private void Start()
     {
         // Find the player object in the scene (tagged as "Player")
+        mainGameObject = GameObject.FindGameObjectWithTag("Main");
         player = GameObject.FindGameObjectWithTag("Player");
 
         if (player == null)
@@ -22,6 +23,35 @@ public class RoomSwapper : MonoBehaviour
         if (mainGameObject == null)
         {
             Debug.LogError("Main Game Object is not assigned!");
+        }
+    }
+
+    public void Interact()
+    {
+        StartCoroutine(WaitForInput());
+    }
+
+    private IEnumerator WaitForInput()
+    {
+        Debug.Log("Press 0 to cancel or 1 to swap rooms.");
+
+        bool responded = false;
+
+        while (!responded)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0))
+            {
+                Debug.Log("0 pressed: Interaction canceled.");
+                responded = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                Debug.Log("1 pressed: Calling SwapRoom.");
+                SwapRoom();
+                responded = true;
+            }
+
+            yield return null; // Wait for the next frame
         }
     }
 
@@ -46,6 +76,7 @@ public class RoomSwapper : MonoBehaviour
         {
             // Move the player to the target position in the new scene
             player.transform.position = playerTargetPositionInScene2;
+            player.GetComponent<PlayerMovement>().targetPosition = playerTargetPositionInScene2;
         }
         else
         {
