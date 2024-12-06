a107a51336475fe64b01afcf3f38e1b1789d2a51
Peakkay <prachet.09.10@gmail.com>
2024-12-06 06:04:57 +0530

    Base complete

--- Assets/Scripts/RoomEnder.cs
+++ Assets/Scripts/RoomEnder.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class RoomEnder : MonoBehaviour
+public class RoomEnder : MonoBehaviour, IInteractable
 {
     public Vector3 playerTargetPositionInScene2; // Target position for the player in Scene 2
     public string targetSceneName; // The name of the scene to load
